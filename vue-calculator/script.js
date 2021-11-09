9cd0eba3ea6d6d1a989f4d62a775c54f93936861
Julia Kirschenheuter <Julia.Kirschenheuter@gmail.com>
2021-11-09 09:25:41 +0100

    vue-calculator was improved

--- vue-calculator/script.js
+++ vue-calculator/script.js
@@ -1,4 +1,24 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js';
+
+const buttons = [
+    {
+        value: 'sum',
+        symbol: '➕'
+    },
+    {
+        value: 'subtract',
+        symbol: '➖'
+    },
+    {
+        value: 'multiply',
+        symbol: '✖'
+    },
+    {
+        value: 'divide',
+        symbol: '➗'
+    }
+];
+
 const app = new Vue({
     template: `#app`,
     
@@ -7,55 +27,37 @@ const app = new Vue({
             firstNumber: 0,
             secondNumber: 0,
             // resultValue: 0,
-            valueRadioButton: '',
+            operation: 'sum'
         }
     },
     
     computed: {
-        resultValue() {
-            this.firstNumber = Number(this.firstNumber);
-            this.secondNumber = Number(this.secondNumber);
-            if (this.valueRadioButton === 'sum') {
-                return this.firstNumber + this.secondNumber
-            } else if (this.valueRadioButton === 'subtract') {
-                return this.firstNumber - this.secondNumber;
-            } else if (this.valueRadioButton === 'multiply') {
-                return this.firstNumber * this.secondNumber
-            } else if (this.valueRadioButton === 'divide') {
-                return this.firstNumber / this.secondNumber;
+        buttons() {
+            return buttons.map((button, i) => ({
+                ...button,
+                key: `keyFor_${i}`
+            }))
+        },
+        firstNumberAsNumber() {
+            return Number(this.firstNumber);
+        },
+        secondNumberAsNumber() {
+            return Number(this.secondNumber);
+        },
+        result() {
+            if (this.operation === 'sum') {
+                return this.firstNumberAsNumber + this.secondNumberAsNumber
+            } else if (this.operation === 'subtract') {
+                return this.firstNumberAsNumber - this.secondNumberAsNumber;
+            } else if (this.operation === 'multiply') {
+                return this.firstNumberAsNumber * this.secondNumberAsNumber
+            } else if (this.operation === 'divide') {
+                return this.firstNumberAsNumber / this.secondNumberAsNumber;
             }
             return 0
         }
     }
-    // watch: {
-    //     valueRadioButton: function(newVal) {
-    //         this.valueRadioButton = newVal;
-    //         this.calculateValue(this.firstNumber, this.secondNumber)
-    //     },
-    //     firstNumber: function(newVal) {
-    //         this.firstNumber = newVal;
-    //         this.calculateValue(this.firstNumber, this.secondNumber)
-    //     },
-    //     secondNumber: function(newVal) {
-    //         this.secondNumber = newVal;
-    //         this.calculateValue(this.firstNumber, this.secondNumber)
-    //     }
-    // },
-    // methods: {
-    //     calculateValue(a, b) {
-    //         a = Number(a);
-    //         b = Number(b);
-    //         if (this.valueRadioButton === 'sum') {
-    //             this.resultValue = a + b;
-    //         } else if (this.valueRadioButton === 'subtract') {
-    //             this.resultValue = a - b;
-    //         } else if (this.valueRadioButton === 'multiply') {
-    //             this.resultValue = a * b;
-    //         } else if (this.valueRadioButton === 'divide') {
-    //             this.resultValue = a / b;
-    //         }
-    //     }
-    // }
+
 })
 
 app.$mount('#app');
